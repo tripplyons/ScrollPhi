4c1e3e40c425e43c7f5514c618eb52774330989f
Tripp Lyons <tripplyons@gmail.com>
2016-03-25 17:01:03 -0700

    .

---
+++ sprite/ScrollingSprite.java
@@ -0,0 +1,11 @@
+package sprite;
+
+public class ScrollingSprite extends Sprite {
+	public ScrollingSprite(int x, int y) {
+		super(x, y);
+	}
+	
+	public void draw() {
+		
+	}
+}
--- src/sprite/ScrollingSprite.java
+++
@@ -1,9 +0,0 @@
-package sprite;
-
-public class ScrollingSprite extends Sprite {
-	public ScrollingSprite() {
-		super(x, y)
-	}
-	
-	public void draw()
-}
